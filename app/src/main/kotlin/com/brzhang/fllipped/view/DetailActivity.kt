476279dfd7ad393c039531d3f8cd6ffe4eb09c94
hoollyzhang <hoollyzhang@tencent.com>
2017-08-02 10:58:52 +0800

    【新增】显示了评论数据。

--- app/src/main/kotlin/com/brzhang/fllipped/view/DetailActivity.kt
+++ app/src/main/kotlin/com/brzhang/fllipped/view/DetailActivity.kt
@@ -20,8 +20,14 @@ import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
 import com.afollestad.materialdialogs.MaterialDialog
 import android.R.id.input
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import com.brzhang.fllipped.model.Comment
+import com.brzhang.fllipped.model.CommentListResponse
 import com.brzhang.fllipped.model.Content
+import com.brzhang.fllipped.pref.UserPref
 
 
 /**
@@ -54,6 +60,10 @@ class DetailActivity : FlippedBaseActivity(), OnPreparedListener {
 
     private var mLlComment: RelativeLayout? = null
 
+    private var mRecyclerView: RecyclerView? = null
+
+    private var adapter: CommentAdapter? = null
+
     override fun handleRxEvent(event: Any?) {
     }
 
@@ -73,6 +83,7 @@ class DetailActivity : FlippedBaseActivity(), OnPreparedListener {
         mImage = flipped_detail_image
         mVideo = flipped_detail_video
         mLlComment = flipped_detail_comment_ll
+        mRecyclerView = flipped_detail_comment_recycler_view
         mLlComment?.setOnClickListener({
             MaterialDialog.Builder(this)
                     .title("请输入评论内容")
@@ -88,6 +99,9 @@ class DetailActivity : FlippedBaseActivity(), OnPreparedListener {
                     }).show()
         })
         mVideo?.setOnPreparedListener(this)
+        mRecyclerView?.layoutManager = LinearLayoutManager(this)
+        adapter = CommentAdapter()
+        mRecyclerView?.adapter = adapter
         showNavigationBack()
     }
 
@@ -99,6 +113,9 @@ class DetailActivity : FlippedBaseActivity(), OnPreparedListener {
                 .subscribe(object : Subscriber<Comment>() {
                     override fun onNext(t: Comment?) {
                         toast("评论成功")
+                        comment.id = t?.id
+                        comment.uid = UserPref.getUserName(applicationContext, "无名小辈")
+                        adapter?.addComment(comment)
                     }
 
                     override fun onCompleted() {
@@ -137,6 +154,25 @@ class DetailActivity : FlippedBaseActivity(), OnPreparedListener {
                         hideProgressBar()
                     }
                 })
+        fllippedNetService().listComments(mFlippedId)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(object : Subscriber<CommentListResponse>() {
+                    override fun onNext(t: CommentListResponse?) {
+                        adapter?.comments = t?.comments ?: arrayListOf()
+                        adapter?.notifyDataSetChanged()
+                    }
+
+                    override fun onCompleted() {
+                        hideProgressBar()
+                    }
+
+                    override fun onError(e: Throwable?) {
+                        toast(e?.message ?: "查询评论失败")
+                        hideProgressBar()
+                    }
+                })
+
     }
 
     private fun showFlippedDetail(t: Flippedword?) {
@@ -166,6 +202,34 @@ class DetailActivity : FlippedBaseActivity(), OnPreparedListener {
             mVideo?.visibility = View.VISIBLE
             mVideo?.setVideoURI(Uri.parse(FlippedHelper.getVideo(t)))
         }
+    }
+
+    inner class CommentAdapter : RecyclerView.Adapter<CommentViewHolder>() {
+        var comments: ArrayList<Comment> = arrayListOf()
+
+        fun addComment(comment: Comment) {
+            comments.add(comment)
+            notifyItemInserted(itemCount)
+        }
+
+        override fun onBindViewHolder(holder: CommentViewHolder?, position: Int) {
+            holder?.send?.text = """${comments[position].uid}："""
+            holder?.contentText?.text = comments[position].contents?.get(0)?.text
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): CommentViewHolder {
+            return CommentViewHolder(LayoutInflater.from(parent?.context).inflate(R.layout.flipped_comment_item, parent, false))
+        }
+
+        override fun getItemCount(): Int {
+            return comments.size
+        }
+
+    }
+
+    inner class CommentViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+        var send: TextView = itemView?.findViewById(R.id.flipped_comment_item_tv_sender) as TextView
+        var contentText: TextView = itemView?.findViewById(R.id.flipped_comment_item_tv_content_text) as TextView
     }
 
     companion object {
